81d8a53beac07a0c30e3d65820c588793e7d53c1
thuongpp <thuongphi003@gmail.com>
2023-07-16 22:13:42 +0700

    add code nhé :)))

--- trangchu.js
+++ trangchu.js
@@ -41,3 +41,43 @@ for (let item of products.data) {
 
     document.getElementById("products").appendChild(card)
 }
+
+const products2 = {
+    data: [
+        {
+            image: "/image/quangcao_hinh1.jpeg",
+        },
+        {
+            image: "/image/quangcao_hinh9.jpeg",
+        },
+        {
+            image: "/image/quangcao_hinh3.png",
+        },
+        {
+            image: "/image/quangcao_hinh4.png",
+        },
+        {
+            image: "/image/quangcao_hinh5.jpeg",
+        },
+        {
+            image: "/image/quangcao_hinh6.jpeg",
+        },
+        {
+            image: "/image/quangcao_hinh7.png",
+        },
+        {
+            image: "/image/quangcao_hinh8.jpeg",
+        },
+    ]
+}
+
+for (let item of products2.data) {
+    let card2 = document.createElement("div")
+    card2.classList.add("slider-item")
+    
+    let image = document.createElement("img")
+    image.setAttribute("src", item["image"])
+    card2.appendChild(image)
+
+    document.getElementById("products2").appendChild(card2)
+}
